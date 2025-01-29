53fab99b1cb892a125a60388847bf5a1852143cc
Jeronimocifci <111008942+Jeronimocifci@users.noreply.github.com>
2025-01-29 09:55:38 -0500

    Add files via upload

---
+++ Lab_machine/pyexmple.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Crear una matriz de 8x8 con 1 en la diagonal principal y 0 en los demás elementos
+matriz = np.eye(8)
+
+# Imprimir la matriz
+print("Matriz 8x8 con 1 en la diagonal principal:")
+print(matriz)
+
+
